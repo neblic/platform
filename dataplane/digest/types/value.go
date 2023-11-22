65fdca7b8ed6ff08e8cc32bec14473a6a6502fc9
Jordi Vilaseca <jordivilaseca@users.noreply.github.com>
2023-11-22 08:43:55 +0100

    feat(dataplane): export HyperLogLog and Cardinality

---
+++ dataplane/digest/types/value.go
@@ -0,0 +1,493 @@
+package types
+
+import (
+	"errors"
+	"math"
+	"strconv"
+
+	"github.com/axiomhq/hyperloglog"
+	"github.com/neblic/platform/dataplane/protos"
+)
+
+type MinStat struct {
+	Value float64
+}
+
+func NewMinStat() *MinStat {
+	return &MinStat{
+		Value: math.Inf(+1),
+	}
+}
+
+func NewMinStatFromProto(minStat *protos.MinStat) *MinStat {
+	return &MinStat{
+		Value: minStat.Value,
+	}
+}
+
+func (ms *MinStat) ToProto() *protos.MinStat {
+	return &protos.MinStat{
+		Value: ms.Value,
+	}
+}
+
+type AvgStat struct {
+	Sum   float64
+	Count uint64
+}
+
+func NewAvgStat() *AvgStat {
+	return &AvgStat{
+		Sum:   0,
+		Count: 0,
+	}
+}
+
+func NewAvgStatFromProto(avgStat *protos.AvgStat) *AvgStat {
+	return &AvgStat{
+		Sum:   avgStat.Sum,
+		Count: avgStat.Count,
+	}
+}
+
+func (as *AvgStat) ToProto() *protos.AvgStat {
+	return &protos.AvgStat{
+		Sum:   as.Sum,
+		Count: as.Count,
+	}
+}
+
+type MaxStat struct {
+	Value float64
+}
+
+func NewMaxStat() *MaxStat {
+	return &MaxStat{
+		Value: math.Inf(-1),
+	}
+}
+
+type HyperLogLog struct {
+	Sketch      *hyperloglog.Sketch
+	Cardinality uint64
+}
+
+func NewHyperLogLog() *HyperLogLog {
+	return &HyperLogLog{
+		Sketch:      hyperloglog.New14(),
+		Cardinality: 0,
+	}
+}
+
+func NewHyperLogLogFromProto(hyperLogLog *protos.HyperLogLog) (*HyperLogLog, error) {
+	sketch := hyperloglog.New14()
+	err := sketch.UnmarshalBinary(hyperLogLog.Data)
+
+	return &HyperLogLog{
+		Sketch:      sketch,
+		Cardinality: hyperLogLog.Cardinality,
+	}, err
+}
+
+func (hll *HyperLogLog) InsertBytes(data []byte) *HyperLogLog {
+	hll.Sketch.Insert(data)
+
+	return hll
+}
+
+func (hll *HyperLogLog) InsertInt64(data int64) *HyperLogLog {
+	hll.Sketch.Insert([]byte(strconv.FormatInt(data, 10)))
+
+	return hll
+}
+
+func (hll *HyperLogLog) InsertFloat64(data float64) *HyperLogLog {
+	hll.Sketch.Insert([]byte(strconv.FormatFloat(data, 'E', -1, 64)))
+
+	return hll
+}
+
+func (hll *HyperLogLog) ToProto() *protos.HyperLogLog {
+	// Sketch MarshalBinary returns an error to follow the encoding.BinaryMarshaler interface,
+	// but it never returns an error.
+	data, _ := hll.Sketch.MarshalBinary()
+
+	return &protos.HyperLogLog{
+		Data:        data,
+		Cardinality: hll.Sketch.Estimate(),
+	}
+}
+
+func NewMaxStatFromProto(maxStat *protos.MaxStat) *MaxStat {
+	return &MaxStat{
+		Value: maxStat.Value,
+	}
+}
+
+func (ms *MaxStat) ToProto() *protos.MaxStat {
+	return &protos.MaxStat{
+		Value: ms.Value,
+	}
+}
+
+type NumberStat struct {
+	Min         *MinStat
+	Avg         *AvgStat
+	Max         *MaxStat
+	HyperLogLog *HyperLogLog
+}
+
+func NewNumberStat() *NumberStat {
+	return &NumberStat{
+		Min:         NewMinStat(),
+		Avg:         NewAvgStat(),
+		Max:         NewMaxStat(),
+		HyperLogLog: NewHyperLogLog(),
+	}
+}
+
+func NewNumberStatFromProto(numberStat *protos.NumberStat) (*NumberStat, error) {
+	hyperLogLog, err := NewHyperLogLogFromProto(numberStat.HyperLogLog)
+
+	return &NumberStat{
+		Min:         NewMinStatFromProto(numberStat.Min),
+		Avg:         NewAvgStatFromProto(numberStat.Avg),
+		Max:         NewMaxStatFromProto(numberStat.Max),
+		HyperLogLog: hyperLogLog,
+	}, err
+}
+
+func (ns *NumberStat) ToProto() *protos.NumberStat {
+	return &protos.NumberStat{
+		Min:         ns.Min.ToProto(),
+		Avg:         ns.Avg.ToProto(),
+		Max:         ns.Max.ToProto(),
+		HyperLogLog: ns.HyperLogLog.ToProto(),
+	}
+}
+
+type NumberValue struct {
+	TotalCount   uint64
+	DefaultCount uint64
+	NullCount    uint64
+	Min          *MinStat
+	Avg          *AvgStat
+	Max          *MaxStat
+	HyperLogLog  *HyperLogLog
+}
+
+func NewNumberValue() *NumberValue {
+	return &NumberValue{
+		TotalCount:   0,
+		DefaultCount: 0,
+		NullCount:    0,
+
+		Min:         NewMinStat(),
+		Avg:         NewAvgStat(),
+		Max:         NewMaxStat(),
+		HyperLogLog: NewHyperLogLog(),
+	}
+}
+
+func NewNumberValueFromProto(numberValue *protos.NumberValue) (*NumberValue, error) {
+	hyperLogLog, err := NewHyperLogLogFromProto(numberValue.HyperLogLog)
+
+	return &NumberValue{
+		TotalCount:   numberValue.TotalCount,
+		DefaultCount: numberValue.DefaultCount,
+		NullCount:    numberValue.NullCount,
+
+		Min:         NewMinStatFromProto(numberValue.Min),
+		Avg:         NewAvgStatFromProto(numberValue.Avg),
+		Max:         NewMaxStatFromProto(numberValue.Max),
+		HyperLogLog: hyperLogLog,
+	}, err
+}
+
+func (nv *NumberValue) ToProto() *protos.NumberValue {
+	return &protos.NumberValue{
+		TotalCount:   nv.TotalCount,
+		DefaultCount: nv.DefaultCount,
+		NullCount:    nv.NullCount,
+
+		Min:         nv.Min.ToProto(),
+		Avg:         nv.Avg.ToProto(),
+		Max:         nv.Max.ToProto(),
+		HyperLogLog: nv.HyperLogLog.ToProto(),
+	}
+}
+
+type StringValue struct {
+	TotalCount   uint64
+	DefaultCount uint64
+	NullCount    uint64
+	HyperLogLog  *HyperLogLog
+	Length       *NumberStat
+}
+
+func NewStringValue() *StringValue {
+	return &StringValue{
+		TotalCount:   0,
+		DefaultCount: 0,
+		NullCount:    0,
+		HyperLogLog:  NewHyperLogLog(),
+		Length:       NewNumberStat(),
+	}
+}
+
+func NewStringValueFromProto(stringValue *protos.StringValue) (*StringValue, error) {
+	hyperLogLog, err := NewHyperLogLogFromProto(stringValue.HyperLogLog)
+	length, errLength := NewNumberStatFromProto(stringValue.Length)
+
+	return &StringValue{
+		TotalCount:   stringValue.TotalCount,
+		DefaultCount: stringValue.DefaultCount,
+		NullCount:    stringValue.NullCount,
+
+		HyperLogLog: hyperLogLog,
+
+		Length: length,
+	}, errors.Join(err, errLength)
+}
+
+func (sv *StringValue) ToProto() *protos.StringValue {
+	return &protos.StringValue{
+		TotalCount:   sv.TotalCount,
+		DefaultCount: sv.DefaultCount,
+		NullCount:    sv.NullCount,
+
+		HyperLogLog: sv.HyperLogLog.ToProto(),
+
+		Length: sv.Length.ToProto(),
+	}
+}
+
+type BooleanValue struct {
+	TotalCount   uint64
+	DefaultCount uint64
+	NullCount    uint64
+	FalseCount   uint64
+	TrueCount    uint64
+}
+
+func NewBooleanValue() *BooleanValue {
+	return &BooleanValue{
+		TotalCount:   0,
+		DefaultCount: 0,
+		NullCount:    0,
+
+		FalseCount: 0,
+		TrueCount:  0,
+	}
+}
+
+func NewBooleanValueFromProto(booleanValue *protos.BooleanValue) *BooleanValue {
+	return &BooleanValue{
+		TotalCount:   booleanValue.TotalCount,
+		DefaultCount: booleanValue.DefaultCount,
+		NullCount:    booleanValue.NullCount,
+
+		FalseCount: booleanValue.FalseCount,
+		TrueCount:  booleanValue.TrueCount,
+	}
+}
+
+func (bv *BooleanValue) ToProto() *protos.BooleanValue {
+	return &protos.BooleanValue{
+		TotalCount:   bv.TotalCount,
+		DefaultCount: bv.DefaultCount,
+		NullCount:    bv.NullCount,
+
+		FalseCount: bv.FalseCount,
+		TrueCount:  bv.TrueCount,
+	}
+}
+
+type ArrayValue struct {
+	TotalCount   uint64
+	DefaultCount uint64
+	NullCount    uint64
+	Values       *ValueValue
+}
+
+func NewArrayValue() *ArrayValue {
+	return &ArrayValue{
+		TotalCount:   0,
+		DefaultCount: 0,
+		NullCount:    0,
+
+		Values: NewValueValue(),
+	}
+}
+
+func NewArrayValueFromProto(arrayValue *protos.ArrayValue) (*ArrayValue, error) {
+	values, err := NewValueValueFromProto(arrayValue.Values)
+
+	return &ArrayValue{
+		TotalCount:   arrayValue.TotalCount,
+		DefaultCount: arrayValue.DefaultCount,
+		NullCount:    arrayValue.NullCount,
+
+		Values: values,
+	}, err
+}
+
+func (av *ArrayValue) ToProto() *protos.ArrayValue {
+	return &protos.ArrayValue{
+		TotalCount:   av.TotalCount,
+		DefaultCount: av.DefaultCount,
+		NullCount:    av.NullCount,
+
+		Values: av.Values.ToProto(),
+	}
+}
+
+type ObjValue struct {
+	TotalCount   uint64
+	DefaultCount uint64
+	NullCount    uint64
+	Fields       map[string]*ValueValue
+}
+
+func NewObjValue() *ObjValue {
+	return &ObjValue{
+		TotalCount:   0,
+		DefaultCount: 0,
+		NullCount:    0,
+
+		Fields: map[string]*ValueValue{},
+	}
+}
+
+func NewObjValueFromProto(objValue *protos.ObjValue) (*ObjValue, error) {
+	fields := map[string]*ValueValue{}
+	var errs error
+	for key, value := range objValue.Fields {
+		var err error
+		fields[key], err = NewValueValueFromProto(value)
+		errs = errors.Join(errs, err)
+	}
+
+	return &ObjValue{
+		TotalCount:   objValue.TotalCount,
+		DefaultCount: objValue.DefaultCount,
+		NullCount:    objValue.NullCount,
+
+		Fields: fields,
+	}, errs
+}
+
+func (ov *ObjValue) ToProto() *protos.ObjValue {
+	fields := map[string]*protos.ValueValue{}
+	for key, value := range ov.Fields {
+		fields[key] = value.ToProto()
+	}
+
+	return &protos.ObjValue{
+		TotalCount:   ov.TotalCount,
+		DefaultCount: ov.DefaultCount,
+		NullCount:    ov.NullCount,
+
+		Fields: fields,
+	}
+}
+
+type ValueValue struct {
+	TotalCount uint64
+	NullCount  uint64
+	Number     *NumberValue
+	String     *StringValue
+	Boolean    *BooleanValue
+	Array      *ArrayValue
+	Obj        *ObjValue
+}
+
+func NewValueValue() *ValueValue {
+	return &ValueValue{
+		TotalCount: 0,
+		NullCount:  0,
+
+		Number:  nil,
+		String:  nil,
+		Boolean: nil,
+		Array:   nil,
+		Obj:     nil,
+	}
+}
+
+func NewValueValueFromProto(valueValue *protos.ValueValue) (*ValueValue, error) {
+	var errs error
+
+	var number *NumberValue
+	if valueValue.Number != nil {
+		var err error
+		number, err = NewNumberValueFromProto(valueValue.Number)
+		errs = errors.Join(errs, err)
+	}
+
+	var string *StringValue
+	if valueValue.String_ != nil {
+		var err error
+		string, err = NewStringValueFromProto(valueValue.String_)
+		errs = errors.Join(errs, err)
+	}
+
+	var array *ArrayValue
+	if valueValue.Array != nil {
+		var err error
+		array, err = NewArrayValueFromProto(valueValue.Array)
+		errs = errors.Join(errs, err)
+	}
+
+	var obj *ObjValue
+	if valueValue.Obj != nil {
+		var err error
+		obj, err = NewObjValueFromProto(valueValue.Obj)
+		errs = errors.Join(errs, err)
+	}
+
+	return &ValueValue{
+		TotalCount: valueValue.TotalCount,
+		NullCount:  valueValue.NullCount,
+
+		Number:  number,
+		String:  string,
+		Boolean: NewBooleanValueFromProto(valueValue.Boolean),
+		Array:   array,
+		Obj:     obj,
+	}, errs
+}
+
+func (vv *ValueValue) ToProto() *protos.ValueValue {
+	var number *protos.NumberValue
+	if vv.Number != nil {
+		number = vv.Number.ToProto()
+	}
+	var string *protos.StringValue
+	if vv.String != nil {
+		string = vv.String.ToProto()
+	}
+	var boolean *protos.BooleanValue
+	if vv.Boolean != nil {
+		boolean = vv.Boolean.ToProto()
+	}
+	var array *protos.ArrayValue
+	if vv.Array != nil {
+		array = vv.Array.ToProto()
+	}
+	var obj *protos.ObjValue
+	if vv.Obj != nil {
+		obj = vv.Obj.ToProto()
+	}
+
+	return &protos.ValueValue{
+		TotalCount: vv.TotalCount,
+		NullCount:  vv.NullCount,
+
+		Number:  number,
+		String_: string,
+		Boolean: boolean,
+		Array:   array,
+		Obj:     obj,
+	}
+}
--- dataplane/digest/value.go
+++ dataplane/digest/value.go
@@ -6,7 +6,7 @@ import (
 	"math"
 
 	"github.com/neblic/platform/controlplane/control"
-	"github.com/neblic/platform/dataplane/protos"
+	"github.com/neblic/platform/dataplane/digest/types"
 	"github.com/neblic/platform/internal/pkg/data"
 	"google.golang.org/protobuf/encoding/protojson"
 )
@@ -27,7 +27,7 @@ type Value struct {
 	maxProcessedFields int
 
 	fieldsProcessed int
-	digest          *protos.ObjValue
+	digest          *types.ObjValue
 }
 
 func (v *Value) isDigest() {}
@@ -36,24 +36,24 @@ func NewValue(maxProcessedFields int) *Value {
 	return &Value{
 		maxProcessedFields: maxProcessedFields,
 
-		digest: protos.NewObjValue(),
+		digest: types.NewObjValue(),
 	}
 }
 
-func (v *Value) updateMinStat(state *protos.MinStat, value float64) {
+func (v *Value) updateMinStat(state *types.MinStat, value float64) {
 	state.Value = math.Min(state.Value, value)
 }
 
-func (v *Value) updateAvgStat(state *protos.AvgStat, value float64) {
+func (v *Value) updateAvgStat(state *types.AvgStat, value float64) {
 	state.Sum += value
 	state.Count++
 }
 
-func (v *Value) updateMaxStat(state *protos.MaxStat, value float64) {
+func (v *Value) updateMaxStat(state *types.MaxStat, value float64) {
 	state.Value = math.Max(state.Value, value)
 }
 
-func (v *Value) updateBoolean(state *protos.BooleanValue, boolean *bool) (*protos.BooleanValue, error) {
+func (v *Value) updateBoolean(state *types.BooleanValue, boolean *bool) (*types.BooleanValue, error) {
 	if err := v.incrFieldsProcessed(); err != nil {
 		return nil, err
 	}
@@ -73,7 +73,7 @@ func (v *Value) updateBoolean(state *protos.BooleanValue, boolean *bool) (*proto
 	return state, nil
 }
 
-func (v *Value) updateNum(state *protos.NumberValue, number *float64) (*protos.NumberValue, error) {
+func (v *Value) updateNum(state *types.NumberValue, number *float64) (*types.NumberValue, error) {
 	if err := v.incrFieldsProcessed(); err != nil {
 		return nil, err
 	}
@@ -85,6 +85,7 @@ func (v *Value) updateNum(state *protos.NumberValue, number *float64) (*protos.N
 		v.updateMinStat(state.Min, *number)
 		v.updateAvgStat(state.Avg, *number)
 		v.updateMaxStat(state.Max, *number)
+		state.HyperLogLog.InsertFloat64(*number)
 	} else {
 		state.NullCount++
 	}
@@ -94,7 +95,7 @@ func (v *Value) updateNum(state *protos.NumberValue, number *float64) (*protos.N
 
 }
 
-func (v *Value) updateString(state *protos.StringValue, str *string) (*protos.StringValue, error) {
+func (v *Value) updateString(state *types.StringValue, str *string) (*types.StringValue, error) {
 	if err := v.incrFieldsProcessed(); err != nil {
 		return nil, err
 	}
@@ -103,9 +104,11 @@ func (v *Value) updateString(state *protos.StringValue, str *string) (*protos.St
 		if *str == "" {
 			state.DefaultCount++
 		}
+		state.HyperLogLog.InsertBytes([]byte(*str))
 		v.updateMinStat(state.Length.Min, float64(len(*str)))
 		v.updateAvgStat(state.Length.Avg, float64(len(*str)))
 		v.updateMaxStat(state.Length.Max, float64(len(*str)))
+		state.Length.HyperLogLog.InsertInt64(int64(len(*str)))
 
 	} else {
 		state.NullCount++
@@ -115,7 +118,7 @@ func (v *Value) updateString(state *protos.StringValue, str *string) (*protos.St
 	return state, nil
 }
 
-func (v *Value) updateArray(state *protos.ArrayValue, array []interface{}) (*protos.ArrayValue, error) {
+func (v *Value) updateArray(state *types.ArrayValue, array []interface{}) (*types.ArrayValue, error) {
 	if err := v.incrFieldsProcessed(); err != nil {
 		return nil, err
 	}
@@ -140,7 +143,7 @@ func (v *Value) updateArray(state *protos.ArrayValue, array []interface{}) (*pro
 	return state, nil
 }
 
-func (v *Value) updateObj(state *protos.ObjValue, m map[string]interface{}) (*protos.ObjValue, error) {
+func (v *Value) updateObj(state *types.ObjValue, m map[string]interface{}) (*types.ObjValue, error) {
 	if err := v.incrFieldsProcessed(); err != nil {
 		return nil, err
 	}
@@ -151,7 +154,7 @@ func (v *Value) updateObj(state *protos.ObjValue, m map[string]interface{}) (*pr
 			valueState, ok := state.Fields[key]
 			if !ok {
 				nonNullCounter := state.TotalCount - state.NullCount
-				valueState = protos.NewValueValue()
+				valueState = types.NewValueValue()
 				valueState.NullCount = nonNullCounter
 				valueState.TotalCount = nonNullCounter
 				state.Fields[key] = valueState
@@ -185,7 +188,7 @@ func (v *Value) updateObj(state *protos.ObjValue, m map[string]interface{}) (*pr
 	return state, nil
 }
 
-func (v *Value) updateValue(state *protos.ValueValue, jsonInterface interface{}) (*protos.ValueValue, error) {
+func (v *Value) updateValue(state *types.ValueValue, jsonInterface interface{}) (*types.ValueValue, error) {
 	if err := v.incrFieldsProcessed(); err != nil {
 		return nil, err
 	}
@@ -198,7 +201,7 @@ func (v *Value) updateValue(state *protos.ValueValue, jsonInterface interface{})
 		if state.Boolean == nil {
 			// First time a boolean is registered in this digest. The number of times this digest was seen is
 			// stored in the structure digest statistics, use it as a reference to track how many times this digest was null
-			state.Boolean = protos.NewBooleanValue()
+			state.Boolean = types.NewBooleanValue()
 			state.Boolean.NullCount = state.TotalCount
 			state.Boolean.TotalCount = state.TotalCount
 		}
@@ -217,7 +220,7 @@ func (v *Value) updateValue(state *protos.ValueValue, jsonInterface interface{})
 		if state.Number == nil {
 			// First time a float is registered in this digest. The number of times this digest was seen is
 			// stored in the structure digest statistics, use it as a reference to track how many times this digest was null
-			state.Number = protos.NewNumberValue()
+			state.Number = types.NewNumberValue()
 			state.Number.NullCount = state.TotalCount
 			state.Number.TotalCount = state.TotalCount
 		}
@@ -233,15 +236,15 @@ func (v *Value) updateValue(state *protos.ValueValue, jsonInterface interface{})
 
 	case string:
 		// Initialize string digest if necessary. Update otherwise
-		if state.String_ == nil {
+		if state.String == nil {
 			// First time a string is registered in this digest. The number of times this digest was seen is
 			// stored in the structure digest statistics, use it as a reference to track how many times this digest was null
-			state.String_ = protos.NewStringValue()
-			state.String_.NullCount = state.TotalCount
-			state.String_.TotalCount = state.TotalCount
+			state.String = types.NewStringValue()
+			state.String.NullCount = state.TotalCount
+			state.String.TotalCount = state.TotalCount
 		}
 
-		_, err := v.updateString(state.String_, &jsonValue)
+		_, err := v.updateString(state.String, &jsonValue)
 		if err != nil {
 			return state, err
 		}
@@ -255,7 +258,7 @@ func (v *Value) updateValue(state *protos.ValueValue, jsonInterface interface{})
 		if state.Array == nil {
 			// First time an array is registered in this digest. The number of times this digest was seen is
 			// stored in the structure digest statistics, use it as a reference to track how many times this digest was null
-			state.Array = protos.NewArrayValue()
+			state.Array = types.NewArrayValue()
 			state.Array.NullCount = state.TotalCount
 			state.Array.TotalCount = state.TotalCount
 		}
@@ -272,7 +275,7 @@ func (v *Value) updateValue(state *protos.ValueValue, jsonInterface interface{})
 	case map[string]interface{}:
 		// Initialize map digest if necessary. Update otherwise
 		if state.Obj == nil {
-			state.Obj = protos.NewObjValue()
+			state.Obj = types.NewObjValue()
 			state.Obj.NullCount = state.TotalCount
 			state.Obj.TotalCount = state.TotalCount
 		}
@@ -301,9 +304,9 @@ func (v *Value) updateValue(state *protos.ValueValue, jsonInterface interface{})
 		state.Number.NullCount++
 		state.Number.TotalCount++
 	}
-	if computedDigestType != StringValueType && state.String_ != nil {
-		state.String_.NullCount++
-		state.String_.TotalCount++
+	if computedDigestType != StringValueType && state.String != nil {
+		state.String.NullCount++
+		state.String.TotalCount++
 	}
 	if computedDigestType != BooleanValueType && state.Boolean != nil {
 		state.Boolean.NullCount++
@@ -350,7 +353,7 @@ func (v *Value) AddSampleData(sampleData *data.Data) error {
 }
 
 func (v *Value) JSON() ([]byte, error) {
-	json, err := protojson.Marshal(v.digest)
+	json, err := protojson.Marshal(v.digest.ToProto())
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal digest to json: %w", err)
 	}
@@ -359,7 +362,7 @@ func (v *Value) JSON() ([]byte, error) {
 }
 
 func (v *Value) Reset() {
-	v.digest = protos.NewObjValue()
+	v.digest = types.NewObjValue()
 }
 
 func (v *Value) String() string {
